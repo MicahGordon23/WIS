ee27f6d1808b8576dab096c3a78cb085b3e2f432
Micah Gordon <micah.gordon@oit.edu>
2023-09-30 15:57:41 -0700

    added new component Overview

--- WIS/source/GrainInvSys/src/app/app.module.ts
+++ WIS/source/GrainInvSys/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
