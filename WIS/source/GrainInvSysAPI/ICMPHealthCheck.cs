acbecfdc59eb4669d3e1c92ee4eef649a4fea494
Micah Gordon <micah.gordon@oit.edu>
2023-10-01 14:06:54 -0700

    Added ICMP health check to check

---
+++ WIS/source/GrainInvSysAPI/ICMPHealthCheck.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net.NetworkInformation;
+namespace GrainInvSysAPI
+{ 
+    public class ICMPHealthCheck : IHealthCheck
+    {
+        private readonly string Host = $"10.0.0.0";
+        private readonly int HealthyRoundtripTime = 100;
+        
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, 
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using Ping ping = new Ping();
+                PingReply reply = await ping.SendPingAsync(Host);
+                switch (reply.Status)
+                {
+                    case IPStatus.Success:
+                        return (reply.RoundtripTime > HealthyRoundtripTime) 
+                            ? HealthCheckResult.Degraded() : HealthCheckResult.Healthy();
+                    default:
+                        return HealthCheckResult.Unhealthy();
+                }
+            }
+            catch (Exception e) 
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+        }
+    }
+}
