b0a31db9a10376b76ecf7c3740c1fff390229f6b
Micah Gordon <micah.gordon@oit.edu>
2023-12-28 15:26:47 -0800

    Generate boiler plate for Home, Nav, and Warehouse components

--- WIS/source/WIS_Prototype/WIS_Prototype/src/app/app.module.ts
+++ WIS/source/WIS_Prototype/WIS_Prototype/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarehouseComponent } from './warehouse/warehouse.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarehouseComponent,
+    HomeComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
