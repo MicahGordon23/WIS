de335cfd51c689f30b47646c6e4464e8dee85408
Micah Gordon <micah.gordon@oit.edu>
2023-12-28 15:27:28 -0800

    Added weightsheet and load Controllers

---
+++ WIS/source/WIS_Prototype/WIS_PrototypeAPI/Controllers/LoadController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WIS_PrototypeAPI.DbContexts;
+using WIS_PrototypeAPI.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WIS_PrototypeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoadController : ControllerBase
+    {
+        // database context for DI
+        private readonly MasterContext _context;
+
+        // Ctor with DI for data context
+        public LoadController(MasterContext context)
+        {
+            _context = context;
+        }
+
+        //****************************************
+        // Checks for existing load in database
+        private bool LoadExists(long id)
+        {
+            return _context.Loads.Any(e => e.LoadId == id);
+        }
+
+        //****************************************
+        // Gets ALL Loads in the data base
+        [HttpGet]
+        public async Task<ActionResult<List<Load>>> GetAll()
+        {
+            return Ok(await _context.Loads.ToListAsync());
+        }
+
+        //****************************************
+        // Create new Load
+        [HttpPost]
+        public async Task<ActionResult<Load>> Post(Load load)
+        {
+            _context.Loads.Add(load);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Get", new { id = load.LoadId }, load);
+        }
+
+        //****************************************
+        // Update existing Load
+        [HttpPut]
+        public async Task<IActionResult> Put(long id, Load load)
+        {
+            if (id !=load.LoadId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(load).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                if (!LoadExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ WIS/source/WIS_Prototype/WIS_PrototypeAPI/Controllers/WeightsheetController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WIS_PrototypeAPI.Data.Models;
+using WIS_PrototypeAPI.DbContexts;
+
+namespace WIS_PrototypeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeightsheetController : ControllerBase
+    {
+        // Database context reference for DI
+        private readonly MasterContext _context;
+
+        // Ctor with DI of database context
+        public WeightsheetController(MasterContext context)
+        {
+            _context = context;
+        }
+
+        //****************************************
+        // Checks for existing load in database
+        private bool WeightsheetExists(long id)
+        {
+            return _context.Weightsheets.Any(w => w.WeightsheetId == id);
+        }
+
+        //****************************************
+        // Gets ALL weightsheets
+        [HttpGet]
+        public async Task<ActionResult<List<Weightsheet>>> GetAllWeightsheets()
+        {
+            return Ok(await _context.Weightsheets.ToListAsync());
+        }
+
+        //****************************************
+        // Create new WeightSheet
+        [HttpPost]
+        public async Task<ActionResult<Weightsheet>> PostWeightsheet(Weightsheet weightsheet)
+        {
+            _context.Weightsheets.Add(weightsheet);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetWeightsheet", new { id = weightsheet.WeightsheetId }, weightsheet);
+        }
+
+        //****************************************
+        // Update existing Load
+        [HttpPut]
+        public async Task<IActionResult> PutWeightsheet(long id, Load load)
+        {
+            if (id != load.LoadId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(load).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WeightsheetExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+    }
+}
