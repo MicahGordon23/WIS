2a84e7349da6b0e52476c53533fe2249bb4439ab
Micah Gordon <micah.gordon@oit.edu>
2024-01-01 18:13:34 -0800

    Basic table working on the frontend

--- WIS/source/WIS_Prototype/WIS_Prototype/src/app/weightsheet/weightsheet.component.ts
+++ WIS/source/WIS_Prototype/WIS_Prototype/src/app/weightsheet/weightsheet.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { Weightsheet } from './weightsheet';
+
 
 @Component({
   selector: 'app-weightsheet',
   templateUrl: './weightsheet.component.html',
   styleUrls: ['./weightsheet.component.scss']
 })
-export class WeightsheetComponent {
+export class WeightsheetComponent implements OnInit {
+  public weightsheets!: Weightsheet[];
 
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get<Weightsheet[]>('/api/weightsheet')
+      .subscribe(result => {
+        this.weightsheets = result;
+      }, error => console.error(error));
+  }
 }
