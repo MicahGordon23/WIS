137b538af5f3c0ae858b3a840918841a58ccde1c
Micah Gordon <micah.gordon@oit.edu>
2024-01-10 15:17:03 -0800

    Refactored ApiResult for Prototypes

--- WIS/source/WIS_Prototype/WIS_PrototypeAPI/Data/ApiResult.cs
+++ WIS/source/WIS_Prototype/WIS_PrototypeAPI/Data/ApiResult.cs
@@ -36,23 +36,17 @@ namespace WIS_PrototypeAPI.Data
                 return ((PageIndex + 1) < TotalPages);
             }
         }
-        private ApiResult(List<T> data, int count, int pageIndex, int pageSize) 
+        private ApiResult(List<T> data) 
         {
             Data = data;
-            PageIndex = pageIndex;
-            PageSize = pageSize;
-            TotalCount = count;
-            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
         }
         
         //****************************************
         // Purpose: Creates an Api request to the 
-        public static async Task<ApiResult<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        public static async Task<ApiResult<T>> CreateAsync(IQueryable<T> source)
         {
-            var count = await source.CountAsync();
-            source = source.Skip(pageIndex * pageSize).Take(pageSize);
             var data = await source.ToListAsync();
-            return new ApiResult<T>(data, count, pageIndex, pageSize);
+            return new ApiResult<T>(data);
         }
     }
 }
