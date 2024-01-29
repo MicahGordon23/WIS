317a2aef6a21f023e6bab5be7b1a1dad62e3772e
Micah Gordon <micah.gordon@oit.edu>
2024-01-28 23:21:24 -0800

    Prelim set up for new weightsheets

--- WIS/source/WIS_Prototype/WIS_Prototype/src/app/weightsheet/new-weightsheet.component.ts
+++ WIS/source/WIS_Prototype/WIS_Prototype/src/app/weightsheet/new-weightsheet.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 import { MatDialogRef, MatDialog } from '@angular/material/dialog';
 import { DialogConfig } from '@angular/cdk/dialog';
 import { FormControl, FormGroup } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 
 import { NewLotComponent } from '../lot/new-lot.component'
-import { Weightsheet } from './weightsheet';
+
+import { IWeightsheet, Weightsheet, NewWeightsheet, NewWeightsheetNoVariety } from './weightsheet';
+import { WeightsheetService } from './weightsheet.service';
+
+import { CommodityType } from '../commodity-type/commodity-type';
+import { CommodityTypeService } from '../commodity-type/commodity-type.service';
+
+import { CommodityVariety } from '../commodity-variety/commodity-variety';
+import { CommodityVarietyService } from '../commodity-variety/commodity-variety.service';
+
+import { Lot } from '../lot/lot';
+import { LotService } from '../lot/lot.service';
 
 @Component({
   selector: 'app-new-weightsheet',
@@ -16,12 +26,25 @@ export class NewWeightsheetComponent {
   constructor(
     private lotDialog: MatDialog,
     private dialogRef: MatDialogRef<NewWeightsheetComponent>,
-    private http: HttpClient
+    private weightsheetService: WeightsheetService,
+    private commodityTypeService: CommodityTypeService,
+    private commodityVarietyService: CommodityVarietyService,
+    private lotService: LotService,
   ) { }
 
   form!: FormGroup;
 
-  weightsheet!: Weightsheet;
+  // Weightsheet which will hold the form
+  weightsheet!: IWeightsheet;
+
+  // Holds list of lots
+  lots?: Lot[];
+
+  // Holds array of commodity types
+  commodities!: CommodityType[];
+
+  // Holds array of commodity varieties
+  varieties?: CommodityVariety[];
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -33,15 +56,55 @@ export class NewWeightsheetComponent {
     })
   }
 
+  //**********************************************
+  // Purpose: Opens a new Dialog window containting New Lot form.
   openNewLotDialog(): void {
     const dialogConfig = new DialogConfig();
     let dialogRef = this.lotDialog.open(NewLotComponent, {});
   }
 
-  onSubmit() {
-
+  //**********************************************
+  // Purpose: When a Commodity Type is selected in the form, the variety select field is
+  //    populated with its varieties.
+  onSelect(event: Event) {
+    const typeId = Number((event.target as HTMLInputElement).value);
+    this.commodityVarietyService.getByType(typeId)
+      .subscribe(result => this.varieties = result);
   }
 
+  //**********************************************
+  // Purpose: Validates and Submits New Weightsheet form.
+  onSubmit() {
+    let w = new IWeightsheet();
+
+    w.weightSheetId = this.form.controls['weightsheetId'].value;
+    w.commodityTypeIdLink = this.form.controls['commodityType'].value;
+    //w.warehouseIdLink = GetWarehouseId();
+    w.warehouseIdLink = 1;
+    w.weigher = this.form.controls['weigher'].value;
+    w.hauler = this.form.controls['miles'].value;
+    w.billOfLading = this.form.controls['billOfLading'].value;
+    w.notes = this.form.controls['notes'].value;
+
+    // Variety Checking.
+    if (this.form.controls['commodityVariety'].value != '') {
+      let _ws = new NewWeightsheet();
+      _ws = w;
+      _ws.commodityVarietyIdLink = this.form.controls['commodityVariety'].value;
+      // Cast w to NewWeightsheet
+      w = _ws;
+    }
+
+    w.dateOpened = new Date();
+
+    this.weightsheetService.post(this.weightsheet)
+      .subscribe(result => {
+        console.log("Weightsheet Added")
+      }, error => console.log(error));
+  }
+
+  //**********************************************
+  // Closes the New Weightsheet dialog window
   onCancel(): void {
     this.dialogRef.close();
   }
