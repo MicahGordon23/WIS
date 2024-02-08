9d1dca4709d9f6f3cf5fce8a19d091d05001c7a6
Micah Gordon <micah.gordon@oit.edu>
2024-02-07 21:52:06 -0800

    Added basic Controller and started a DTO for Intake Report.

---
+++ WIS/source/WIS_Prototype/WIS_PrototypeAPI/Controllers/ReportsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WIS_PrototypeAPI.Data;
+using WIS_PrototypeAPI.Data.Models;
+
+namespace WIS_PrototypeAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ReportsController : ControllerBase
+	{
+		private readonly AppDbContext _context;
+
+		public ReportsController (AppDbContext context)
+		{
+			_context = context;
+		}
+
+		// GET: api/Reports/Intake
+		[HttpGet("/Intake")]
+		public async Task<ActionResult>
+	}
+}
---
+++ WIS/source/WIS_Prototype/WIS_PrototypeAPI/Data/DTOs/IntakeReport.cs
@@ -0,0 +1,36 @@
+﻿//************************************************
+// Filename: IntakeReport.cs
+// Purpose: Defines an object which is used to transport Report data from the database.
+
+using WIS_PrototypeAPI.Data;
+using WIS_PrototypeAPI.Data.Models;
+
+namespace WIS_PrototypeAPI.Data.DTOs
+{
+	public class IntakeReport
+	{
+		// Commodity Type
+
+		// Commodity Variety
+
+		// Producer number
+
+		// Producer name
+
+		// Lot number
+
+		// lot is open or closed
+
+		// Landlord
+
+		// FSA farm number
+
+		// Net (lbs)
+
+		// Net UoM (bu or CWT)
+
+		// Sum of Net
+
+		// Sum of UoM
+	}
+}
