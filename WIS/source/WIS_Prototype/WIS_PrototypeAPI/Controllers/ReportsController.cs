70af0e2e104072c56a181be4407cc7b97ae34d25
Micah Gordon <micah.gordon@oit.edu>
2024-02-18 17:07:52 -0800

    Skeletal Report done in backend

--- WIS/source/WIS_Prototype/WIS_PrototypeAPI/Controllers/ReportsController.cs
+++ WIS/source/WIS_Prototype/WIS_PrototypeAPI/Controllers/ReportsController.cs
@@ -4,6 +4,9 @@ using Microsoft.EntityFrameworkCore;
 using WIS_PrototypeAPI.Data;
 using WIS_PrototypeAPI.Data.Models;
 using WIS_PrototypeAPI.Data.DTOs;
+using Humanizer;
+using NuGet.Packaging.Signing;
+using System.Data.Common;
 
 namespace WIS_PrototypeAPI.Controllers
 {
@@ -18,31 +21,82 @@ namespace WIS_PrototypeAPI.Controllers
 			_context = context;
 		}
 
-		//// GET: api/Reports/Intake
-		//[HttpGet("/Intake/{warehouseId}")]
-		//public async Task<ActionResult<IntakeReport>> GetIntakeReport(int warehouseId)
-		//{
-		//	if (_context.Warehouses == null)
-		//	{
-		//		return NotFound();
-		//	}
-		//	// Weightsheet where WarehouseIdLink == warehouseId && weightsheet.DateClosed is == DateNow
-		//	// Lots Where
-		//	IntakeReport intakeReport = new IntakeReport();
-		//	var report = await _context.Weightsheets
-		//		.Where(Weightsheets => Weightsheets.WarehouseIdLInk == warehouseId && Weightsheets.DateClosed == null)
-		//		.ToListAsync();
-		//	// Weightsheet had the
-		//	// Commodity Type and Variety Id and Name
-		//	// Producer Id and Name
-		//	// Weightsheet Id
-		//	// Lot Id
-		//	// Landlord
-		//	// FSA 
-		//	// Net
-		//	// Net UoM
+		// GET: api/Reports/Intake
+		[HttpGet("Intake/{warehouseId}")]
+		public async Task<ActionResult<IntakeReport>> GetIntakeReport(int warehouseId)
+		{
+			// Error checking for missing tables. "should not happen"
+			if (_context.Warehouses == null)
+			{
+				return NotFound();
+			}
+			if (_context.Lots == null)
+			{
+				return NotFound();
+			}
+			if (_context.Weightsheets == null)
+			{
+				return NotFound();
+			}
+			if (_context.Loads == null)
+			{
+				return NotFound();
+			}
+			// Triple join from Lots (where WarehouseIdLink == wearhouseID) on Weightsheets (Where Date == todays Date) on Loads
+			/*	Key Information:
+			 *	Purpose of Intake report is to track intake from producers into the warehouse.
+			 *	For each Weightsheet:
+			 *		Commodity Type, Commodity Variety,
+			 *		From Lot: Producer Name, Producer Number, Lot Number, Landlord, FSA Number
+			 *		For each Load on weightsheet
+			 *			From Load: Net weight per load
+			 *	
+			 * SELECT Weightsheets.WeightSheetId, SUM(Loads.NetWeight) AS TotalWeight, Weightsheets.DateOpened
+			 *	FROM Weightsheets
+			 *	INNER JOIN Lots
+			 *	ON Lots.LotId = Weightsheets.WeightSheetId
+			 *	INNER JOIN Loads
+			 *	ON Loads.WeightsheetIdLink = Weightsheets.WeightSheetId
+			 *	GROUP BY WeightSheetId, Weightsheets.DateOpened;
+			 */
 
+			// I have chosen to ignore the UoM for the moment and abstracted it to just bu for the moment.
+			//var intakeReport = from Lots in _context.Set<Lot>()
+			//				   join Weightsheets in _context.Set<Weightsheet>()
+			//					   on new { Lots.LotId, Weightsheets.LotIdLink } equals new { Lots.WarehouseIdLink,  warehouseIdLink = 1 }
 
-		//}
+			var today = DateTime.Now.Date;
+
+			var query = from weightsheet in _context.Weightsheets
+						join lot in _context.Lots on weightsheet.WeightSheetId equals lot.LotId
+						join load in _context.Loads on weightsheet.WeightSheetId equals load.WeightsheetIdLink
+						where lot.WarehouseIdLink == warehouseId && weightsheet.DateClosed == today
+						group new { weightsheet, load } 
+						by new 
+						{
+							//lot.Producer,
+							weightsheet.LotIdLink,
+							weightsheet.DateOpened,
+							weightsheet.WeightSheetId,
+							//weightsheet.CommodityType,
+							//weightsheet.CommodityVariety,
+							lot.Landlord,
+							lot.FarmNumber
+						} into grouped
+						select new IntakeReport
+						{
+							//producer = grouped.Key.Producer,
+							LotNumber = (long)grouped.Key.LotIdLink,
+							WeightsheetId = grouped.Key.WeightSheetId,
+							DateLotOpened = (DateTime)grouped.Key.DateOpened,
+							//CommodityType = grouped.Key.CommodityType,
+							//CommodityVariety = grouped.Key.CommodityVariety,
+							Landlord = grouped.Key.Landlord,
+							FarmNumber = grouped.Key.FarmNumber,
+							NetWeightLbs = (long)grouped.Sum(x => x.load.NetWeight)
+						};
+			var result = await query.ToListAsync();
+			return Ok(result);
+		}
 	}
 }
--- WIS/source/WIS_Prototype/WIS_PrototypeAPI/Data/DTOs/IntakeReport.cs
+++ WIS/source/WIS_Prototype/WIS_PrototypeAPI/Data/DTOs/IntakeReport.cs
@@ -9,44 +9,43 @@ namespace WIS_PrototypeAPI.Data.DTOs
 {
 	public class IntakeReport
 	{
-		// Commodity Type
-		public int commodityTypeId { get; set; }
-		public string? commodityTypeName { get; set; } = null;
+		//// Commodity Type
+		//public int CommodityTypeId { get; set; }
+		//public string? CommodityTypeName { get; set; } = null;
 
-		// Commodity Variety
-		public long commodityVarietyId { get; set; }
-		public string? commodityVarietyName { get; set; } = null;
+		//// Commodity Variety
+		//public long CommodityVarietyId { get; set; }
+		//public string? CommodityVarietyName { get; set; } = null;
 
-		// Producer number
-		public int producerId { get; set; }
-		// Producer name
-		public string? producerName { get; set;} = null;
+		//// Producer number
+		//public int ProducerId { get; set; }
+		//// Producer name
+		//public string? ProducerName { get; set;} = null;
+
+		public Producer? producer { get; set; }
 
 		// Lot number
-		public long lotNumber { get; set; }
+		public long LotNumber { get; set; }
 
 		// Weight Sheet number
-		public long WeightsheetNumber { get; set; }
+		public long WeightsheetId { get; set; }
 
 		// lot is open or closed
-		public bool isClosed { get; set; }
+		public DateTime DateLotOpened{ get; set; }
+
+		// Commodity Type
+		public CommodityType? CommodityType { get; set; }
+
+		// Commodity Variety
+		public CommodityVariety? CommodityVariety { get; set; }
 
 		// Landlord
-		public string? landlord { get; set; } = null;
+		public string? Landlord { get; set; } = null;
 
 		// FSA farm number
-		public string? farmNumber { get; set; } = null;
+		public string? FarmNumber { get; set; } = null;
 
 		// Net (lbs)
-		public long netWeightLbs { get; set; }
-
-		// Net UoM (bu or CWT)
-		public long netUnitsOfMeasure { get; set; }
-
-		// Sum of Net
-		public long sumNet { get; set; }
-
-		// Sum of UoM
-		public long sumUnitsOfMeasure { get; set; }
+		public long NetWeightLbs { get; set; }
 	}
 }
