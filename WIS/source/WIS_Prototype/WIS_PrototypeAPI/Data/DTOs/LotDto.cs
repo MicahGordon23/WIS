e0b432e5fb11187a9c6e4883b7703c90e08fdb90
Micah Gordon <micah.gordon@oit.edu>
2024-02-27 13:29:22 -0800

    Added backend LotDto and Endpoint for more editable lots

---
+++ WIS/source/WIS_Prototype/WIS_PrototypeAPI/Data/DTOs/LotDto.cs
@@ -0,0 +1,40 @@
+﻿// Filename: LotDto.cs
+// Purpose: Allow for usability of edit functionality.
+// Author: Micah Gordon
+// Date: 02/27/2024
+
+// Updates: <date>:<change>
+using Microsoft.EntityFrameworkCore;
+using WIS_PrototypeAPI.Data;
+using WIS_PrototypeAPI.Data.Models;
+
+namespace WIS_PrototypeAPI.Data.DTOs
+{
+	[Keyless]
+	public class LotDto
+	{
+		public long LotId { get; set; }
+
+		public string? StateId { get; set; } = null;
+
+		public DateTime StartDate { get; set; }
+
+		public DateTime? EndDate { get; set; } = null;
+
+		public string? Landlord { get; set; } = null;
+
+		public string? FarmNumber { get; set; } = null;
+
+		public string? Notes { get; set; } = null;
+
+		public int CommodityTypeIdLink { get; set; }
+		public string? CommodityTypeName { get; set; } = null;
+		
+
+		public long? CommodityVarietyIdLink { get; set; }
+		public string? CommodityVarietyName { get; set; } = null;
+
+		public int ProducerIdLink { get; set; }
+		public string? ProducerName { get; set; } = null;	
+	}
+}
