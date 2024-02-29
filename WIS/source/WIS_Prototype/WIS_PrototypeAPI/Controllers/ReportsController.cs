d65261daf038b73a23dc95cbac7a5ec9040d2a41
Micah Gordon <micah.gordon@oit.edu>
2024-02-29 05:41:11 -0800

    Added 2 arg ctor for Commodity Type and Variety. Added Weight Sheet Report to controler.

--- WIS/source/WIS_Prototype/WIS_PrototypeAPI/Controllers/ReportsController.cs
+++ WIS/source/WIS_Prototype/WIS_PrototypeAPI/Controllers/ReportsController.cs
@@ -24,7 +24,7 @@ namespace WIS_PrototypeAPI.Controllers
 			_context = context;
 		}
 
-		// GET: api/Reports/Intake
+		// GET: api/Reports/Intake/5
 		[HttpGet("Intake/{warehouseId}")]
 		public async Task<ActionResult<IntakeReport>> GetIntakeReport(int warehouseId)
 		{
@@ -90,6 +90,39 @@ namespace WIS_PrototypeAPI.Controllers
 							Landlord = grouped.Key.Landlord,
 							FarmNumber = grouped.Key.FarmNumber,
 							NetWeightLbs = (long)grouped.Sum(x => x.load.NetWeight)
+						};
+			var result = await query.ToListAsync();
+			return Ok(result);
+		}
+
+		// GET api/Reports/DailyWeightSheet/5
+		[HttpGet("Daily/Weightsheet/{warehouseId}")]
+		public async Task<ActionResult<WeightSheetReport>> GetDailyWeightSheetReport(int warehouseId)
+		{
+			var today = DateTime.Now.Date;
+			var query = from weightsheets in _context.Weightsheets
+						join commodity in _context.CommodityTypes on weightsheets.CommodityTypeIdLink equals commodity.CommodityTypeId
+						join variety in _context.CommodityVarieties on weightsheets.CommodityVarietyIdLink equals variety.CommodityVarietyId
+						into commodityPair
+						from variety in commodityPair.DefaultIfEmpty()
+						join load in _context.Loads on weightsheets.WeightSheetId equals load.WeightsheetIdLink
+						where weightsheets.WarehouseIdLink == warehouseId && weightsheets.DateOpened == today
+						group new { weightsheets, commodity, variety, load }
+						by new
+						{
+							weightsheets.WeightSheetId,
+							weightsheets.CommodityTypeIdLink,
+							commodity.CommodityTypeName,
+							weightsheets.CommodityVarietyIdLink,
+							variety.CommodityVarietyName
+						} into grouped
+						select new WeightSheetReport
+						{
+							WeightsheetId = (long)grouped.Key.WeightSheetId,
+							CommodityType = new CommodityType((int)grouped.Key.CommodityTypeIdLink, (string)grouped.Key.CommodityTypeName),
+							CommodityVariety = new CommodityVariety((long)grouped.Key.CommodityVarietyIdLink, (string)grouped.Key.CommodityVarietyName),
+							LoadsOnSheet = (int)grouped.Count(),
+							NetWeight = (int)grouped.Sum(l => l.load.NetWeight)
 						};
 			var result = await query.ToListAsync();
 			return Ok(result);
---
+++ WIS/source/WIS_Prototype/WIS_PrototypeAPI/Data/DTOs/WeightSheetReport.cs
@@ -0,0 +1,26 @@
+﻿//************************************************
+// Filename: WeightSheetReport.cs
+// Purpose: Defines an object which is used to transport Weight Sheet Report data from the database.
+using Microsoft.EntityFrameworkCore;
+using WIS_PrototypeAPI.Data;
+using WIS_PrototypeAPI.Data.Models;
+using WIS_PrototypeAPI.Data.DTOs;
+
+namespace WIS_PrototypeAPI.Data.DTOs
+{
+	public class WeightSheetReport
+	{
+		public WeightSheetReport() { }
+
+		public long WeightsheetId { get; set; }
+
+		public CommodityType CommodityType { get; set; }
+
+		public CommodityVariety? CommodityVariety { get; set; } = null;
+
+		public int LoadsOnSheet { get; set; }
+
+		public long NetWeight { get; set; }
+		
+	}
+}
